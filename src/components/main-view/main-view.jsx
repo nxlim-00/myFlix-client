31d44c41d6341162fc79040596439188c078b2cf
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-03 11:36:39 +0200

    Bug fixed

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../main-view/movie-view';
+import { MovieView } from '../movie-view/movie-view';
 
 const MainView = () => {
   const [movies, setMovies] = useState([
