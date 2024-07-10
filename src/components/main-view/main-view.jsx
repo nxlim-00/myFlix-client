e0b2ebf7eb19069520e7f2783bed9daab3b2dce8
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-10 11:46:19 +0200

    Fixed bugs

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -13,6 +13,7 @@ export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
+    if (!token) return;
     fetch('https://myflixx-movie-app-2d5cece4bfb1.herokuapp.com/movies', {
       headers: { Authorization: `Bearer ${token}` },
     })
@@ -21,17 +22,39 @@ export const MainView = () => {
         const moviesFromApi = movies.map((movie) => {
           return {
             id: movie._id,
-            title: movie.title,
-            description: movie.description,
-            image: movie.image,
-            genre: movie.genre,
-            directior: movie.director,
+            title: movie.Title,
+            description: movie.Description,
+            image: movie.ImagePath,
+            genre: movie.Genre.Name,
+            directior: movie.Director.Name,
           };
         });
-        setMovies(moviesFromApi);
+        setMovies(movies);
       });
   }, [token]);
 
+  /* useEffect(() => {
+    if (!token) return;
+    fetch('https://myflixx-movie-app-2d5cece4bfb1.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, [token]);
+
+  const moviesFromApi = movies.map((movie) => {
+    return {
+      id: movie._id,
+      title: movie.title,
+      description: movie.description,
+      image: movie.image,
+      genre: movie.genre,
+      directior: movie.director,
+    };
+  });
+ */
   if (!user) {
     return (
       <>
