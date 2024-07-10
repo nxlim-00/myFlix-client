ac4352c356b937e39e258e2e9dcbe96aca955883
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-10 11:44:27 +0200

    Updated movie-view and added info

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,16 +2,50 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={movie.ImagePath} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
-        <span>Author: </span>
-        <span>{movie.author}</span>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
       </div>
+      <div>
+        <span>Genre: </span>
+        <div>
+          <span>Name: </span>
+          <span>{movie.Genre.Name}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.Genre.Description}</span>
+        </div>
+      </div>
+      <div>
+        <span>Director: </span>
+        <div>
+          <span>Name: </span>
+          <span>{movie.Director.Name}</span>
+        </div>
+        <div>
+          {' '}
+          <span>Bio: </span>
+          <span>{movie.Director.Bio}</span>
+        </div>
+        <div>
+          {' '}
+          <span>Birth: </span>
+          <span>{movie.Director.Birth}</span>
+        </div>
+        <div>
+          {' '}
+          <span>Death: </span>
+          <span>{movie.Director.Death}</span>
+        </div>
+      </div>
+
       <button onClick={onBackClick}>Back</button>
     </div>
   );
