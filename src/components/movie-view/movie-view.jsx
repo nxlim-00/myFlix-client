2081d16c62f23b6489261dcb2501b74c38d045ad
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-16 18:11:29 +0200

    Added useParams and useState
    to find movies by id

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,71 +1,83 @@
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import { useParams } from 'react-router';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+// import Button from 'react-bootstrap/Button';
+// import './movie-view.scss';
 
-export const MovieView = ({ movie, onBackClick }) => {
-  return (
-    <div>
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+  const [movie] = useState(movies.find((mov) => mov._id == movieId));
+
+  if (!movie) return <>Loading...</>;
+  else
+    return (
       <div>
-        <img class="mw-100" src={movie.ImagePath} />
-      </div>
-      <div>
-        <span>
-          <strong>Title: </strong>
-        </span>
-        <span>{movie.Title}</span>
-      </div>
-      <div>
-        <span>
-          <strong>Description: </strong>{' '}
-        </span>
-        <span>{movie.Description}</span>
-      </div>
-      <div>
-        <span>
-          <strong>Genre: </strong>
-        </span>
+        <div>
+          <img className="w-30" src={movie.ImagePath} />
+        </div>
         <div>
           <span>
-            <strong>Name: </strong>
+            <strong>Title: </strong>
           </span>
-          <span>{movie.Genre.Name}</span>
+          <span>{movie.Title}</span>
         </div>
         <div>
           <span>
             <strong>Description: </strong>
           </span>
-          <span>{movie.Genre.Description}</span>
+          <span>{movie.Description}</span>
         </div>
+        <div>
+          <span>
+            <strong>Genre: </strong>
+          </span>
+          <div>
+            <span>
+              <strong>Name: </strong>
+            </span>
+            <span>{movie.Genre.Name}</span>
+          </div>
+          <div>
+            <span>
+              <strong>Description: </strong>
+            </span>
+            <span>{movie.Genre.Description}</span>
+          </div>
+        </div>
+        <div>
+          <span>
+            <strong>Director: </strong>
+          </span>
+          <div>
+            <span>
+              <strong>Name: </strong>
+            </span>
+            <span>{movie.Director.Name}</span>
+          </div>
+          <div>
+            <span>
+              <strong>Bio: </strong>
+            </span>
+            <span>{movie.Director.Bio}</span>
+          </div>
+          <div>
+            <span>
+              <strong>Birth: </strong>
+            </span>
+            <span>{movie.Director.Birth}</span>
+          </div>
+          <div>
+            <span>
+              <strong>Death: </strong>
+            </span>
+            <span>{movie.Director.Death}</span>
+          </div>
+        </div>
+        <Link to={`/`}>
+          <Button variant="primary">Back</Button>
+        </Link>
       </div>
-      <div>
-        <span>
-          <strong>Director: </strong>
-        </span>
-        <div>
-          <span>
-            <strong>Name: </strong>
-          </span>
-          <span>{movie.Director.Name}</span>
-        </div>
-        <div>
-          <span>
-            <strong>Bio: </strong>
-          </span>
-          <span>{movie.Director.Bio}</span>
-        </div>
-        <div>
-          <span>
-            <strong>Birth: </strong>
-          </span>
-          <span>{movie.Director.Birth}</span>
-        </div>
-        <div>
-          <span>
-            <strong>Death: </strong>
-          </span>
-          <span>{movie.Director.Death}</span>
-        </div>
-      </div>
-
-      <Button onClick={onBackClick}>Back</Button>
-    </div>
-  );
+    );
 };
