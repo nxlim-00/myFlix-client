dd530392bc49cf0bc85bc4eeb06a38cedb025389
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-16 22:12:55 +0200

    Added responsive navbar

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,15 +1,25 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export const NavigationBar = ({ user, onLoggedOut }) => {
+export const NavigationBar = ({ user }) => {
   return (
-    <Navbar bg="primary" data-bs-theme="light">
+    <Navbar
+      collapseOnSelect
+      expand={'lg'}
+      sticky="top"
+      bg="primary"
+      data-bs-theme="dark"
+      className="py-3"
+    >
       <Container>
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/" className="me-auto">
           myFlix
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          className="justify-content-md-center"
+          id="responsive-navbar-nav"
+        >
           <Nav className="me-auto">
             {!user && (
               <>
@@ -26,11 +36,25 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/">
                   Home
                 </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                <Nav.Link
+                  to="#"
+                  onClick={() => {
+                    localStorage.clear();
+                    location.reload();
+                    location.href = '/';
+                  }}
+                >
+                  Logout
+                </Nav.Link>
               </>
             )}
           </Nav>
         </Navbar.Collapse>
+        {user && (
+          <Navbar.Text className="ms-auto">
+            Logged in as: <a href="#login">{user.Username}</a>
+          </Navbar.Text>
+        )}
       </Container>
     </Navbar>
   );
