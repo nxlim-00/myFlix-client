843552f6bf7730c7331f60eb59a0c8d969060712
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-16 18:12:19 +0200

    Added Link and removed onMovieClick

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,19 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /* import import "./movie-card.scss"; */
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card className="h-100" onClick={() => onMovieClick(movie)}>
+    <Card className="h-30">
       <Card.Img variant="top" src={movie.ImagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
-
-        <Button variant="primary" onClick={() => onMovieClick(movie)}>
-          Open
-        </Button>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+          <Button variant="primary">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -34,5 +35,4 @@ MovieCard.propTypes = {
       Death: PropTypes.string,
     }),
   }),
-  onMovieClick: PropTypes.func.isRequired,
 };
