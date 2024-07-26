a0f224a6e8d1b91d37f29abb8dc9712050038fee
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-26 03:06:20 +0200

    Added padding

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Button, Form, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 export const SignupView = () => {
@@ -37,53 +36,55 @@ export const SignupView = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="SignUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength="3"
-          placeholder="Enter a username."
-        />
-      </Form.Group>
+    <Container style={{ height: '100vh' }}>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="SignUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3"
+            placeholder="Enter a username."
+          />
+        </Form.Group>
 
-      <Form.Group controlId="SignPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          placeholder="Enter a password."
-        />
-      </Form.Group>
-      <Form.Group controlId="SignMail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder="Enter a valid email."
-        />
-      </Form.Group>
+        <Form.Group controlId="SignPassword">
+          <Form.Label style={{ paddingTop: '10px' }}>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Enter a password."
+          />
+        </Form.Group>
+        <Form.Group controlId="SignMail">
+          <Form.Label style={{ paddingTop: '10px' }}>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="Enter a valid email."
+          />
+        </Form.Group>
 
-      <Form.Group controlId="SignBirthday">
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
+        <Form.Group controlId="SignBirthday">
+          <Form.Label style={{ paddingTop: '10px' }}>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
 
-      <Button style={{ marginTop: '20px' }} type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button style={{ marginTop: '20px' }} type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 };
